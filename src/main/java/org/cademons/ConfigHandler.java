2ad1ecd0cf347af91681d6053d71bb8e3ebed928
nammyc <john.namgung@concordacademy.org>
2018-01-14 10:32:26 -0500

    Added template

---
+++ src/main/java/org/cademons/ConfigHandler.java
@@ -0,0 +1,4 @@
+package org.cademons;
+
+public class ConfigHandler {
+}
---
+++ src/main/java/org/cademons/Launcher.java
@@ -0,0 +1,10 @@
+package org.cademons;
+
+import org.bytedeco.javacpp.presets.freenect;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+    }
+}
