50153998e7594395671ab97824700f3974d88e78
nammyc <john.namgung@concordacademy.org>
2018-01-14 11:01:26 -0500

    added kinect test

--- src/main/java/org/cademons/KinectHandler.java
+++ src/main/java/org/cademons/KinectHandler.java
@@ -1,4 +1,18 @@
 package org.cademons;
 
+import org.openkinect.freenect.*;
+
 public class KinectHandler {
+    // Declarations
+    Device device = null;
+    Context context = null;
+
+    public KinectHandler() {
+        this.context = Freenect.createContext();
+        if(this.context.numDevices() > 0) {
+            this.device = this.context.openDevice(0);
+        } else {
+            System.err.println("No kinects detected. Exiting.");
+        }
+    }
 }
