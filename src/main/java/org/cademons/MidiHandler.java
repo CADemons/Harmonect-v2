4bd0f0e00848acd3d5ec7fdcc583c1e6f39419aa
SmeeBoi <32615775+SmeeBoi@users.noreply.github.com>
2018-01-14 11:38:13 -0500

    Finished MidiHandler

--- src/main/java/org/cademons/MidiHandler.java
+++ src/main/java/org/cademons/MidiHandler.java
@@ -1,4 +1,30 @@
-package org.cademons;
+package com.cademons;
+import javax.sound.midi.*;
 
-public class MidiHandler {
+public class MIDIHandler {
+
+    private Synthesizer synth;
+    private MidiChannel[] channels;
+    private short defaultVelocity = 220;
+
+    public MIDIHandler(int instrument)  {
+        try {
+            synth = MidiSystem.getSynthesizer();
+            synth.open();
+            channels = synth.getChannels();
+        } catch (MidiUnavailableException e) {
+
+        }
+    }
+
+    public void changeInstrument(int instrument) {synth.getChannels()[0].programChange(instrument);}
+
+    public void noteOn(short note) {channels[0].noteOn(note, defaultVelocity);}
+    public void noteOn(short note, short velocity) {channels[0].noteOn(note, velocity);}
+
+    public void noteOff(short note) {channels[0].noteOff(note);}
+    public void noteOff(short note, short velocity) {channels[0].noteOff(note, velocity);}
+    public void allNotesOff() {channels[0].allNotesOff();}
+
+
 }
